c8ef2275d8d721591475c83a6266014472d78be4
WolfMAN <aria.azadi.pour@protonmail.com>
2020-11-03 15:38:47 +0330

    connect to database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import {ApolloServer, gql} from "apollo-server";
-import logger from "node-color-log";
+import {join} from 'path';
+import {config} from 'dotenv';
 
-import resolvers from "./resolvers";
+config({
+    path: join(__dirname, '../config.env')
+});
+
+import {ApolloServer, gql} from 'apollo-server';
+import mongoose from 'mongoose';
+import logger from 'node-color-log';
+
+import resolvers from './resolvers';
 import schema from './schema.graphql';
 
 const server = new ApolloServer({
@@ -9,6 +17,22 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen().then(({url}) => {
-    logger.info(`🚀 server is running ar ${url}`);
+const Port = process.env.PORT || 4000;
+server.listen({
+    port: Port
+}).then(({url}) => {
+    logger.info(`🚀 server is running ar ${url}.`);
+});
+
+const DB = (process.env.DATABASE_URL || '')
+    .replace('<password>', process.env.DATABASE_PASSWORD)
+    .replace('<database>', process.env.DATABASE_NAME);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    logger.info('🍃 Connected to database successfully.')
 });
