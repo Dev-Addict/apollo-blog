ef837e644cf96bdb5c48cee6ea1492ec9c731751
WolfMAN <aria.azadi.pour@protonmail.com>
2020-11-05 21:52:10 +0330

    update posts query to only show published posts

--- src/resolvers/queries/posts.js
+++ src/resolvers/queries/posts.js
@@ -1,6 +1,10 @@
 import Post from "../../models/Post";
 import find from "../../helpers/queries/find";
 
-const posts = (parentValues, {page, limit, sort, filter}, context, info) => find(Post, page, limit, sort, filter);
+const posts = (parentValues, {page, limit, sort, filter}, context, info) => {
+    filter.published = true;
+
+    return find(Post, page, limit, sort, filter)
+};
 
 export default posts;
