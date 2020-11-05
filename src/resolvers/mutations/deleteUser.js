0c86ec399d403be382654f800c6935d377b894f5
WolfMAN <aria.azadi.pour@protonmail.com>
2020-11-05 19:54:38 +0330

    delete user posts on deleting account

--- src/resolvers/mutations/deleteUser.js
+++ src/resolvers/mutations/deleteUser.js
@@ -1,9 +1,11 @@
 import User from "../../models/User";
+import Post from "../../models/Post";
 import protect from "../../utils/auth/protect";
 
 const deleteUser = async (parentValues, args, {req}, info) => {
     const {_id} = await protect(req);
 
+    await Post.deleteMany({author: _id});
     await User.findByIdAndDelete(_id);
 
     return {
